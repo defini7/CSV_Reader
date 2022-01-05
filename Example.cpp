77af9797dd56ab49ebd0631972685cb69988e606
Alex <75216201+defini7@users.noreply.github.com>
2022-01-05 23:13:13 +0300

    Create Example.cpp

---
+++ Example.cpp
@@ -0,0 +1,14 @@
+#include "defCSV_Reader.h"
+
+int main()
+{
+	def::CSV_Reader* reader = new def::CSV_Reader("large.csv");
+
+	reader->Print();
+
+	std::cout << "\nTotal Cols: " << reader->GetTotalCols() << "\n" << "Total Rows: " << reader->GetTotalRows() << "\n\n";
+
+	std::cout << reader->GetValue(0, 0) << "\n";
+
+	return 0;
+}
